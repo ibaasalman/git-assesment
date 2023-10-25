446e030e6920d05aded1ad6f3067e6f6a44e061b
ibaa <ibaasalman0@gmail.com>
2023-10-25 19:34:30 +0300

    fix

--- feature.ts
+++ feature.ts
@@ -1,9 +1,4 @@
 // Calculate the sum of two numbers
-<<<<<<< HEAD
 function calculateSum(a: number, b: number): number {
-    return a + b ;
-=======
-function calculateSumm(a: number, b: number): number {
-    return a + b + a ;
->>>>>>> feature-branch
+    return a + b +b+ a ;
 }
